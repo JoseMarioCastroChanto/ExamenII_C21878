0b26def82b8219620ab7d022f0909d8225e3de67
JoseMarioCastroChanto <jose.castrochanto@ucr.ac.cr>
2024-12-04 19:36:29 -0600

    Endpoint to the purchase and change query

---
+++ backend/API/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Application;
+using backend.Domain;
+
+namespace backend.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private  PaymentCommand _command;
+        private PaymentQuery _query;
+
+
+        public PaymentController(PaymentCommand command, PaymentQuery query)
+        {
+            _command = command;
+            _query = query;
+        }
+
+        [HttpGet]
+        public  bool OutOfChange()
+        {
+            var isOutOfChange = _query.OutOfChange();
+            return isOutOfChange;
+        }
+
+        [HttpPut]
+        public IActionResult PaymentTransaction([FromBody] PaymentModel payment)
+        {
+            try
+            {
+                var cashChange = _command.PaymentTransaction(payment);
+                return Ok(cashChange); 
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+    }
+}
