f8b3d4b862537161a1db1ce26c5411039b8052b9
JoseMarioCastroChanto <jose.castrochanto@ucr.ac.cr>
2024-12-04 19:37:21 -0600

    Command and query for Coffee to check existence

---
+++ backend/Application/CoffeeCommand.cs
@@ -0,0 +1,33 @@
+﻿using backend.Infrastructure;
+
+namespace backend.Application
+{
+    public class CoffeeCommand
+    {
+        private CoffeeQuery _coffeeQuery;
+        private CoffeeHandler _coffeeHandler;
+
+        public CoffeeCommand(CoffeeQuery coffeeQuery, CoffeeHandler coffeeHandler)
+        {
+            _coffeeQuery = coffeeQuery;
+            _coffeeHandler = coffeeHandler;
+        }
+
+        public bool UpdateCoffeeAvailability(int [] CoffeeId, int [] Quantity)
+        {
+ 
+            var existingCoffees = _coffeeQuery.CheckCoffeeExistence(CoffeeId);
+
+
+            if (existingCoffees.Count != CoffeeId.Length)
+            {
+                return false;  
+            }
+
+                _coffeeHandler.UpdateCoffeeAvailability(CoffeeId, Quantity);
+         
+
+            return true;
+        }
+    }
+}
